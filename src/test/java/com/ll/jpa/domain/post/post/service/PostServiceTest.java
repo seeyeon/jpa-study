f87563e1ce6f118a95661e3771958fb7aa0f9290
seeyeon <gulf72pr@swu.ac.kr>
2024-12-16 11:27:40 +0900

    use Transactional & Rollback

--- src/test/java/com/ll/jpa/domain/post/post/service/PostServiceTest.java
+++ src/test/java/com/ll/jpa/domain/post/post/service/PostServiceTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -15,6 +17,8 @@ class PostServiceTest {
 
 	@Test
 	@DisplayName("글 2개 생성")
+	@Transactional
+	@Rollback
 	void t1() {
 		postService.write("제목1_test","내용1_test");
 		postService.write("제목2_test","내용2_test");
